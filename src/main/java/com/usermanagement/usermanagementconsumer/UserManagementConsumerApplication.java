effde73e088ee415f40b114a1f46ec259e89409a
addoumi mohammed <addoumi.mohammed@gmail.com>
2023-01-25 19:10:47 +0100

    project initializatioproject initializationn

---
+++ src/main/java/com/usermanagement/usermanagementconsumer/UserManagementConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.usermanagement.usermanagementconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usermanagement/usermanagementconsumer/UserManagementConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usermanagement.usermanagementconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
